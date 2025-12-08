85dc53648c642c74ba67f94d0f64d594b1426ced
ksinuss <kostionovaki@gmail.com>
2025-12-08 18:06:02 +0300

    changes from backend for backend/crud/auth.py

--- backend/crud/auth.py
+++ backend/crud/auth.py
@@ -1,16 +1,25 @@
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
-from .. import models
+from database.models.models import User
 from ..schemas import UserCreate
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def get_user_by_username(db: Session, username: str):
-    return db.query(models.User).filter(models.User.username == username).first()
+    return db.query(User).filter(User.username == username).first()
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+def check_user_exists(db: Session, username: str, email: str):
+    """Проверяет, существует ли пользователь с таким username или email"""
+    return db.query(User).filter(
+        (User.email == email) | (User.username == username)
+    ).first()
 
 def create_user(db: Session, user: UserCreate):
     hashed_password = pwd_context.hash(user.password)
-    db_user = models.User(
+    db_user = User(
         username=user.username,
         email=user.email,
         password_hash=hashed_password
